352cccfc9606f54c424bf7a1757d6a575ba83f5b
Makarand Pande <mak_pande@yahoo.com>
2017-06-06 06:49:25 -0400

    initial commit

---
+++ routes/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+// router.post('/', function (req, res, next) {
+//     var email = req.body.email
+//     var user = new User({
+//       firstName: 'Mack',
+//       lastName: 'Pande',
+//       password: 'abcd1234',
+//       email: email
+//     });
+//     user.save();
+//
+//     res.redirect('/');
+// });
+
+module.exports = router;
